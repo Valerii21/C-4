8ba533e778c4e4ddba7758bc6bab4643ce9c2e5b
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-08-07 13:46:17 +0300

    4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int num;
+int i = 1;
+            bool not = true;
+
+            Console.WriteLine("Введите число:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чётные числа от 1 до " + num);
+            while (i <= num)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.Write(i + ", ");
+                    not = false;
+                }
+                i++;
+            }
+
+            if (not)
+            {
+                Console.WriteLine("Нет чётных чисел!");
+            }
